36cb5bf6d04fb39e8bc47cad6de0a75f516ecd5c
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-03-12 13:00:31 -0400

    Added comments

--- main.ts
+++ main.ts
@@ -4,41 +4,51 @@
  * Created on: Sep 2026
  * This program shows 7 colors.
 */
-//Value
+
+// variables
 let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 
-//clean up
+// clean up
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 pins.digitalWritePin(DigitalPin.P13, 0)
 pins.digitalWritePin(DigitalPin.P14, 0)
 pins.digitalWritePin(DigitalPin.P15, 0)
-//input
+
+// input
 input.onButtonPressed(Button.A, function() {
 
-    
+    // red
     basic.clearScreen()
     basic.showString('Red')
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
+
+    // green
     basic.clearScreen()
     basic.showString('Green')
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P14, 0)    
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    
+    // blue
     basic.clearScreen()
     basic.showString('Blue')
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P15, 0)  
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    
+    // majent  
     basic.clearScreen()
     basic.showString('Majent')
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P15, 0) 
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    
+    // cyan 
     basic.clearScreen()
     basic.showString('Cyan')
     pins.digitalWritePin(DigitalPin.P14, 1)
@@ -46,25 +56,31 @@ input.onButtonPressed(Button.A, function() {
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // yellow
     basic.clearScreen()
     basic.showString('Yellow')
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
-    pins.digitalWritePin(DigitalPin.P14, 0)  
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    // white
     basic.clearScreen()
     basic.showString('White')
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+
+    // black
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     basic.clearScreen()
     
-//Come back to start    
+    // come back to start    
     basic.showIcon(IconNames.Happy)
 })
 
